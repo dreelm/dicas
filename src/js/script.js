0a74246ab0cb5b959afb138a22fe5752f77275e3
Laboratório FIAP <dree1509luiz@gmail.com>
2026-03-31 10:39:06 -0300

    feat: Operadores aritmeticos. logicos e comparaçao

--- src/js/script.js
+++ src/js/script.js
@@ -18,29 +18,53 @@
 //taxaJuros =0.50;
 //console.log(taxaJuros);
 
-var exemplo1="ola dev var";
-console.log(exemplo1);
+// var exemplo1="ola dev var";
+// console.log(exemplo1);
 
-let exemplo2="ola dev let";
-console.log(exemplo2);
+// let exemplo2="ola dev let";
+// console.log(exemplo2);
 
-const exemplo3="ola dev const";
-console.log(exemplo3);
+// const exemplo3="ola dev const";
+// console.log(exemplo3);
 
-let exemplo4="20";
-console.log(exemplo4);
+// let exemplo4="20";
+// console.log(exemplo4);
 
-let exemplo5=true;
-console.log(exemplo5)
+// let exemplo5=true;
+// console.log(exemplo5)
 
-let exemplo6={nome:"fiap"};
-console.log(exemplo6)
+// let exemplo6={nome:"fiap"};
+// console.log(exemplo6)
 
-let exemplo7=["java","python","C#"]
-console.log(exemplo7);
-//variavel indefinida
-let exemplo8;
-console.log(exemplo8);
+// let exemplo7=["java","python","C#"]
+// console.log(exemplo7);
+// //variavel indefinida
+// let exemplo8;
+// console.log(exemplo8);
 
-let exemplo9=null;
-console.log(exemplo9)
+// let exemplo9=null;
+// console.log(exemplo9)
+
+//OPERADORES ARITMÉDICOS
+
+const valor1=20;
+const valor2=10;
+console.log(valor1 + valor2);
+console.log(valor1 / valor2);
+console.log(valor1 - valor2);
+console.log(valor1 * valor2);
+    
+//OPERADORES LÓGICOS
+
+console.log(valor1 > valor2);
+console.log(valor1 > 10 && valor2 != 20)
+console.log(valor2 > 5 || valor1 > 50)
+
+//OPERADORES DE COMPARAÇAO
+console.log(valor1 < valor2);
+console.log(valor1 >= valor2);
+console.log(valor1 <= valor1);
+console.log(valor1 != valor2);
+console.log(valor1 == valor2);
+console.log(valor1 === valor2);
+console.log("o valor é", valor2);
